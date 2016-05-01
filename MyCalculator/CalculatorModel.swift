be66fbe37bae67d5f281fefb7785f2a73b4575c4
Vayne-Lover <406378362@qq.com>
2016-05-01 11:35:23 +0800

    Sync
    ++

--- MyCalculator/CalculatorModel.swift
+++ MyCalculator/CalculatorModel.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+class CalculatorModel{
+    func evaluate(temp:(Double,Double,String)) -> (Double,Double,String) {
+        var inTemp:(Double,Double,String)=temp
+        switch temp.2 {
+        case "+":
+            inTemp.0=temp.0+temp.1
+        case "−":
+            inTemp.0=temp.0-temp.1
+        case "×":
+            inTemp.0=temp.0*temp.1
+        case "÷":
+//            if inTemp.1==0{
+//                inTemp=(0,0,"+")//shuchu
+//                print("Can't /0")
+//                return inTemp
+//            } else {
+//                inTemp.0=temp.0/temp.1
+//            }
+            inTemp.0=temp.0/temp.1
+        default:
+            break
+        }
+        return inTemp
+    }
+}
--- MyCalculator/CalculatorViewController.swift
+++ MyCalculator/CalculatorViewController.swift
@@ -12,7 +12,7 @@ class CalculatorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        NSThread.sleepForTimeInterval(1.0)
         // Do any additional setup after loading the view.
     }
 
@@ -20,8 +20,71 @@ class CalculatorViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var showLabel: UILabel!
+    var notZero:Bool=false
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit=sender.currentTitle!
+        if(notZero){
+            showLabel.text=showLabel.text!+digit
+        } else if notZero==false && digit=="0"{
+            showLabel.text="0"
+        } else {
+        showLabel.text=digit
+        notZero=true
+        }
+    }
+    var result:(Double,Double,String)=(0,0,"+")
+    var resultbak:(Double,Double,String)=(0,0,"+")
+    var labelValue:Double{
+        get{
+            let temp:String?=showLabel.text!
+            if temp != nil {
+                return Double(showLabel.text!)!
+            } else {
+                return 0
+            }
+        }
+        set{
+        showLabel.text="\(newValue)"
+        }
+    }
+    var labelText:String{
+        get{return showLabel.text!}
+        set{showLabel.text=newValue}
+    }
+    @IBAction func operate(sender: UIButton) {
+        if resultbak.0 != 0{
+            let operation=sender.currentTitle!
+            resultbak.1=labelValue
+            result=cm.evaluate(resultbak)
+            result.1=0
+            result.2=operation
+            labelText=operation
+            //labelValue=0
+            notZero=false
+            resultbak=result
+        } else {
+            let operation=sender.currentTitle!
+            result.0=labelValue
+            result.2=operation
+            labelText=operation
+            //labelValue=0
+            notZero=false
+            resultbak=result//0,1 question can  x/0,+0,-0,*0
+        }
+    }
     
-
+    @IBAction func clear() {
+        showLabel.text="0"
+        notZero=false
+        resultbak=(0,0,"+")
+    }
+    var cm=CalculatorModel()
+    @IBAction func equal() {
+        result.1=labelValue
+        result=cm.evaluate(result)
+        labelValue=result.0
+    }
     /*
     // MARK: - Navigation
 
